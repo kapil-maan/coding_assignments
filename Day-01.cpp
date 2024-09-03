5e26a67050b182a3b2b3343eb18d7b1878e60af7
kapil-maan <kapilmaan0123@gmail.com>
2024-09-03 20:56:07 +0530

    day1 my 1st assignment

---
+++ Day-01.cpp
@@ -0,0 +1,68 @@
+// Ques - 01 - Write a C++ program that takes two strings as input from the user and concatenates them. Then, output the concatenated string.
+
+// Sample Input :- Enter the first string: Hello
+// Enter the second string: World
+
+// Sample Output:- Concatenated string: HelloWorld
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str1; 
+    cout<< "Enter the 1st string:- ";
+    cin>> str1;
+    string str2; 
+    cout<<"Enter the 2nd string:- ";
+    cin>> str2;
+
+   cout<<"Concatenated string: "<< str1 + str2 ;
+
+}
+
+
+// Ques-02 -Write a C++ program that takes five grades (integers) as input from the user and calculates the average grade. Output the average grade to the user.
+
+// Sample Input:- Enter grade 1: 85
+// Enter grade 2: 90
+// Enter grade 3: 78
+// Enter grade 4: 92
+// Enter grade 5: 88
+
+// Sample Output:- The average grade is: 86.6
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int sum=0;
+    for(int i=0; i<5; i++){
+        cout<<"Enter grade " << i+1 << ": "; 
+        int n ; cin>> n ;
+        sum +=n;
+    }
+    cout<<"The average grade is: "<<sum/5.0;
+    
+    return 0;
+}
+
+
+
+// Ques-03 - Write a C++ program that takes an integer as input from the user and outputs its square.
+
+// Sample Input:- Enter a number: 7
+
+// Sample Output :- The square of 7 is: 49
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter a number: ";
+    int n; cin>>n;
+
+    cout<<"The square of "<<n<<" is: "<<n*n;
+    return 0;
+
+}
