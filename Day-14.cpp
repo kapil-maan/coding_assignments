1602fc8779e230542df076fdc195a409e9066dec
Kapil Kumar <145493471+kapil-maan@users.noreply.github.com>
2024-09-13 00:30:36 +0530

    Day 14 problem solved

--- Day-14.cpp
+++ Day-14.cpp
@@ -1,5 +1,5 @@
 // Ques 1: Define a car class with properties brand name , model, engine, number of seats and method displayInfo that display the all information of class.
-
+               
 
 #include <iostream>
 #include <string>
